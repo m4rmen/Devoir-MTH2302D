ec4e182953b1a92b245988bf7679799e2c717ead
Adrien Trahan <adrientrahan@Adriens-MBP.ht.home>
2024-11-22 22:49:16 -0500

    Partie 2 C terminée

--- charger.R
+++ charger.R
@@ -87,3 +87,141 @@ var.test(Sales ~ Region, data = mes_donnees)
 
 # Test égalité des moyennes
 t.test(Sales ~ Region, data = mes_donnees, var.equal = TRUE)
+
+
+### -----------------------------  PHASE 2 --------------------------------------------
+
+## ------- c) -------
+
+donnees_modele <- mes_donnees[c("Sales", "Price", "Age")]
+colnames(donnees_modele) <- c("Y", "X1", "X2")
+
+square <- function(x) {
+  return(x^2)
+}
+
+modeles <- list(
+  ## modele 1
+  list(variableGauche="Y", variableDroite="X1", formuleGauche=identity, formuleDroite=identity, transformation_B0=identity),
+  ## modele 2
+  list(variableGauche="Y", variableDroite="X2", formuleGauche=identity, formuleDroite=identity, transformation_B0=identity),
+  ## modele 3
+  list(variableGauche="Y", variableDroite="X1", formuleGauche=identity, formuleDroite=square, transformation_B0=identity),
+  ## modele 4
+  list(variableGauche="Y", variableDroite="X2", formuleGauche=identity, formuleDroite=square, transformation_B0=identity),
+  ## modele 5
+  list(variableGauche="Y", variableDroite="X1", formuleGauche=log, formuleDroite=identity, transformation_B0=exp),
+  ## modele 6
+  list(variableGauche="Y", variableDroite="X2", formuleGauche=log, formuleDroite=identity, transformation_B0=exp),
+  ## modele 7
+  list(variableGauche="Y", variableDroite="X1", formuleGauche=log, formuleDroite=log, transformation_B0=exp),
+  ## modele 8
+  list(variableGauche="Y", variableDroite="X2", formuleGauche=log, formuleDroite=log, transformation_B0=exp)
+)
+
+analyseModeles <- data.frame()
+
+for (i in seq_along(modeles)) {
+  cat("\n\n------- Modèle", i, "-------\n")
+  formuleDroite <- modeles[[i]]$formuleDroite
+  formuleGauche <- modeles[[i]]$formuleGauche
+  variableDroite <- formuleDroite(donnees_modele[[modeles[[i]]$variableDroite]])
+  variableGauche <- formuleGauche(donnees_modele[[modeles[[i]]$variableGauche]])
+  
+  model <- lm(variableGauche ~ variableDroite, data = donnees_modele)
+  
+  ## ------- c) 1: -------
+  b <- coef(model)[2]
+  a <- coef(model)[1]
+  
+  b0 <- modeles[[i]]$transformation_B0(a)
+  b1 <- b
+  coefficients <- data.frame(b0 = as.numeric(b0), b1 = as.numeric(b1))
+  
+  print(coefficients)
+  analyseVariance = anova(model)
+  print(analyseVariance)
+  
+  ## ------- c) 2: -------
+  seuilB0PValue <- 0.05
+  accepteVariance <- analyseVariance[["Pr(>F)"]][1] < seuilPValue
+  accepteVariance
+  analyseVariance[["Pr(>F)"]][1]
+  
+  if (accepteVariance) {
+    cat("\nH0 rejeté, car le seuil (0.05) est supérieur à ", analyseVariance[["Pr(>F)"]][1])
+    cat("\nLe modèle est donc globalement significatif")
+  }else{
+    cat("\nH0 accepté, car le seuil (0.05) est inférieur à ", analyseVariance[["Pr(>F)"]][1])
+    cat("\nLe modèle n'est donc PAS globalement significatif")
+  }
+  
+  ## Test/Analyse de normalité
+  seuilNormalitePValue <- 0.05
+  residus <- residuals(model)
+  normalitePValue <- shapiro.test(residus)$p.value
+  residuNormalementDistribue <- normalitePValue >= seuilNormalitePValue
+  if (residuNormalementDistribue) {
+    cat("Les résidus suivent une distribution normale au seuil", normalitePValue)
+  } else {
+    cat("Les résidus NE suivent PAS une distribution normale au seuil", normalitePValue)
+  }
+  qqnorm(residus, main = c("Normalité des résidus - Modele", i), xlab = NULL, ylab = NULL)
+  qqline(residus, col = "red", lwd = 2)
+  
+  ## Test/Analyse de l'homoscédasticité
+  seuilHomoscédasticitéPValue <- 0.05
+  bp_test <- bptest(model)
+  varianceConstante <- bp_test$p.value >= seuilHomoscédasticitéPValue;
+  if (varianceConstante) {
+    cat("\nLa variance est constante au seuil", seuilHomoscédasticitéPValue)
+  } else {
+    cat("\nLa variance N'est PAS constante au seuil", seuilHomoscédasticitéPValue)
+  }
+  valeurs_predites <- fitted(model)
+  plot(valeurs_predites, residus, 
+       main = c("Résidus vs Valeurs prédites - Modele", i), 
+       xlab = "Valeurs prédites", 
+       ylab = "Résidus", 
+       pch = 19, col = "blue")
+  abline(h = 0, col = "red", lwd = 2)
+  
+  ## Trouver les points atypiques
+  residus_standardises <- rstandard(model)
+  seuil_residus <- 2
+  points_atypiques <- which(abs(residus_standardises) > seuil_residus)
+  points(valeurs_predites[points_atypiques], residus[points_atypiques], 
+         col = alpha("red", 1), pch = 21, cex = 1.5)
+  
+  
+  ## ------- c) 3: -------
+  seuilConfiance <- 0.95
+  conf <- confint(model, level=seuilConfiance)
+  conf_b0 <- modeles[[i]]$transformation_B0(conf[1, ])
+  cat("\n\nB0 est situé au seuil de", seuilConfiance , "dans l'interval: ", conf_b0)
+  
+  conf_b1 <- conf[2, ]
+  cat("\nB1 est situé au seuil de", seuilConfiance , "dans l'interval: ", conf_b1)
+  
+  analyseModeles <- rbind(analyseModeles, data.frame(b0 = b0,
+                                                     b1 = b1,
+                                                     accepteVariance = accepteVariance,
+                                                     pValueVariance = analyseVariance[["Pr(>F)"]][1],
+                                                     residueNormaux = residuNormalementDistribue,
+                                                     pValueNormalite = normalitePValue,
+                                                     varianceConstante = varianceConstante,
+                                                     pValueHomoscédasticité = bp_test$p.value))
+  rownames(analyseModeles)[nrow(analyseModeles)] <- i
+  
+  plot(formuleDroite(donnees_modele[[modeles[[i]]$variableDroite]]),
+       formuleGauche(donnees_modele[[modeles[[i]]$variableGauche]]), 
+       main = "Sales vs Price", 
+       xlab = "Price", 
+       ylab = "Sales", 
+       pch = 19, col = "blue")
+  
+  abline(a, b, col = "red", lwd = 2)
+}
+
+
+
